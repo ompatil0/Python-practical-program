583a8e6f3f1dfc5776a21004716c3f1a59ac995e
Om Patil <patilomh@gmail.com>
2026-02-01 19:37:41 +0530

    Python assignments

--- Task3.py
+++
@@ -1,4 +0,0 @@
-n = int(input("ENter the range : "))
-
-for i in range(1, n + 1):
-    print(i, end="")
--- Task4.py
+++
@@ -1,8 +0,0 @@
-n = 5
-
-for i in range(n):
-    for s in range(i):
-        print(" ", end="")
-    for j in range(n - i):
-        print("*", end=" ")
-    print()
